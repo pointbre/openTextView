f5ab7399d47454dba9289beffeb12bd21f0b8778
자유해결사 <khjde1207@knou.ac.kr>
2021-04-16 12:00:31 +0900

    두가지 어올리는 색 리스트 정리

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,25 @@ void main() {
   // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
   Get.lazyPut(() => MainCtl());
   runApp(GetMaterialApp(
-    theme: ThemeData(dialogTheme: DialogTheme(backgroundColor: Colors.white)),
+    theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF02343F),
+        cardTheme: CardTheme(color: Color(0xFF02343F)),
+        iconTheme: IconThemeData(color: Color(0xFFF0EDCC)),
+        textTheme: TextTheme(
+          headline3: TextStyle(color: Color(0xFFF0EDCC)),
+          headline4: TextStyle(color: Color(0xFFF0EDCC)),
+          headline5: TextStyle(color: Color(0xFFF0EDCC)),
+          headline6: TextStyle(color: Color(0xFFF0EDCC)),
+          headline1: TextStyle(color: Color(0xFFF0EDCC)),
+          headline2: TextStyle(color: Color(0xFFF0EDCC)),
+          bodyText1: TextStyle(color: Color(0xFFF0EDCC)),
+          bodyText2: TextStyle(color: Color(0xFFF0EDCC)),
+          subtitle1: TextStyle(color: Color(0xFFF0EDCC)),
+          subtitle2: TextStyle(color: Color(0xFFF0EDCC)),
+          overline: TextStyle(color: Color(0xFFF0EDCC)),
+          caption: TextStyle(color: Color(0xFFF0EDCC)),
+        ),
+        dialogTheme: DialogTheme(backgroundColor: Colors.white)),
     getPages: [
       GetPage(name: '/', page: () => MainPage()),
     ],
