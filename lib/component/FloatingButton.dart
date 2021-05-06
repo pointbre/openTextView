d60fe5ad49d8e3404e662d97bc6c51faa862b425
khjde <khjde1207@gmail.com>
2021-05-06 13:52:50 +0900

    빌드 sdk 버전 을 28 로 변경,

--- lib/component/FloatingButton.dart
+++ lib/component/FloatingButton.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:audio_service/audio_service.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:open_textview/controller/MainAudio.dart';
 import 'package:open_textview/controller/MainCtl.dart';
@@ -11,6 +12,9 @@ import 'package:path_provider/path_provider.dart';
 
 class FloatingButton extends GetView<MainCtl> {
   TextPlayerTask t = TextPlayerTask();
+
+  FlutterTts tts = FlutterTts();
+  bool bplayTts = false;
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -23,14 +27,36 @@ class FloatingButton extends GetView<MainCtl> {
           return FloatingActionButton(
               elevation: 2.0,
               onPressed: () async {
-                print('onPressed >> :${bplay}');
+                print('onPressed >> :${bplayTts}');
 
-                if (bplay) {
+                // if (bplay) {
+                if (bplayTts) {
+                  bplayTts = false;
                   // print('onStop');
-                  t.onStop();
+                  // t.onStop();
                   // controller.stop();
+                  tts.stop();
                 } else {
+                  bplayTts = true;
                   print('start');
+                  var en = await tts.getEngines;
+                  List vos = await tts.getVoices;
+
+                  print(vos.where((element) => element['locale'] == 'ko-KR'));
+                  await tts.awaitSpeakCompletion(true);
+                  print('awaitSpeakCompletion');
+                  await tts.setSpeechRate(2.8);
+                  print('setSpeechRate');
+                  await tts.setVolume(1.0);
+                  print('setVolume');
+                  await tts.setPitch(1.0);
+                  print('setPitch');
+                  await tts
+                      .setVoice({"name": "ko-KR-language", "locale": "ko-KR"});
+
+                  var aa = await tts
+                      .speak([...controller.contents.getRange(0, 20)].join());
+                  print(aa);
 
                   // AudioService.playbackStateStream.listen((event) {
                   // });
@@ -44,10 +70,10 @@ class FloatingButton extends GetView<MainCtl> {
                   //       "contents": controller.contents
                   //     });
                   // await AudioService.play();
-                  await t.onStart({
-                    ...(controller.config as Map),
-                    "contents": controller.contents
-                  });
+                  // await t.onStart({
+                  //   ...(controller.config as Map),
+                  //   "contents": controller.contents
+                  // });
                   // await t.onPlay();
                   // controller.play();
                 }
